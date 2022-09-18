0a160c6ef37bd5ef444eb1f83fca2b2fcc3f0b31
Tarxos <ftarxos@gmail.com>
2022-09-18 17:19:15 +0300

    add methods to calculate triangle area, and help understand that triangle is rectangular

--- ShapesMath/MathSh.cs
+++ ShapesMath/MathSh.cs
@@ -2,5 +2,37 @@
 
 public static class MathSh
 {
-    public static double CircleArea(double radius) => Math.PI * radius * radius;
+    public static double CircleArea(double radius) => Math.PI * ValueSquare(radius);
+
+    public static double TriangleAreaBySides(double sideA, double sideB, double sideC)
+    {
+        FindGreatestSide(out var greatest, out var lowestSides, sideA, sideB, sideC);
+        if (IsTriangleRectangular(greatest, lowestSides)) return .5 * lowestSides[0] * lowestSides[1];
+        double halfPerimeter = (sideA + sideB + sideC) / 2;
+        return Math.Sqrt(halfPerimeter * (halfPerimeter - sideA) * (halfPerimeter - sideB) * (halfPerimeter - sideC));
+    }
+
+    public static void FindGreatestSide(out double greatest, out double[] lowestSides, params double[] sides)
+    {
+        greatest = sides[0];
+        lowestSides = new double[sides.Length - 1];
+        for (var i = 1; i < sides.Length; i++)
+        {
+            if (greatest >= sides[i])
+            {
+                lowestSides[i-1] = sides[i];
+                continue;
+            }
+
+            lowestSides[i - 1] = greatest;
+            greatest = sides[i];
+        }
+    }
+
+    public static bool IsTriangleRectangular(double greatest, IReadOnlyList<double> lowestSides)
+    {
+        return Math.Abs(ValueSquare(greatest) - (ValueSquare(lowestSides[0]) + ValueSquare(lowestSides[1]))) < 0.0002;
+    }
+
+    public static double ValueSquare(double value) => value * value;
 }
