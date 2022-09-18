eaa6fa997bf435ef541ec04d6616f9f426b18b5e
Tarxos <ftarxos@gmail.com>
2022-09-18 17:56:21 +0300

    add some tests for circle area calculation

--- TestTasksTests/MathShTests.cs
+++ TestTasksTests/MathShTests.cs
@@ -1,11 +1,16 @@
+using static ShapeMath.MathSh;
+
 namespace TestTasksTests
 {
     public class MathShTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(7, 154)]
+        [InlineData(14, 616)]
+        public void ValidCircleArea(double radius, int expectedAreaInInteger)
         {
-
+            var result = Math.Round(CircleArea(radius));
+            Assert.Equal(expectedAreaInInteger, result);
         }
     }
 }
