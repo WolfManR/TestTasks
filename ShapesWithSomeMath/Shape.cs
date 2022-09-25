cfe92dbaaa5c15abc5a64da6597ee03d4761876b
Tarxos <ftarxos@gmail.com>
2022-09-25 15:57:12 +0300

    add Shapes project and add Blanks of shapes

---
+++ ShapesWithSomeMath/Shape.cs
@@ -0,0 +1,21 @@
+﻿namespace ShapesWithSomeMath;
+
+public abstract class Shape
+{
+    public abstract double Area();
+}
+
+public class Circle : Shape
+{
+    public override double Area() => 0;
+}
+
+public class Triangle : Shape
+{
+    public override double Area() => 0;
+}
+
+public class RectangularTriangle : Triangle
+{
+    public override double Area() => 0;
+}
