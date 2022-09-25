320fdbcb0e3b113977a75b3e7bf672e279faae69
Tarxos <ftarxos@gmail.com>
2022-09-25 19:02:11 +0300

    add rectangle shape easy

---
+++ ShapesWithSomeMath/Rectangle.cs
@@ -0,0 +1,15 @@
+﻿namespace ShapesWithSomeMath;
+
+public class Rectangle : Shape
+{
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public override double Area() => Height * Width;
+}
